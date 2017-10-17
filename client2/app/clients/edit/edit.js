0fe3b4cfa93dca9ee12e6721d98c66ec755209d7
Pontjho Mokhonoana <pontjho@entelect.co.za>
2017-10-18 01:59:07 +0200

    Edit client

--- client2/app/clients/edit/edit.js
+++ client2/app/clients/edit/edit.js
@@ -1,12 +1,20 @@
 (function() {
     "use strict";
 
-    angular.module('absa').controller('EditClientController', ['$scope', function($scope) {
+    angular.module('absa').controller('EditClientController', ['$scope', 'ClientService', '$location', '$routeParams', 
+    function($scope, clientService, location, routeParams) {
 
-        $scope.model = {};
+        console.log(routeParams);
+        clientService.get({clientId: routeParams.clientId})
+            .$promise
+            .then((model) => {$scope.model = model;}, (err) => console.error('Error fetching client', err));
 
         $scope.save = function(model) {
-
+            clientService.update({clientId: routeParams.clientId}, model)
+            .$promise
+            .then(function() {
+                location.path('/');
+            }, (reason) => console.error('Error creating user', reason));
         };
     }]);
 })();
