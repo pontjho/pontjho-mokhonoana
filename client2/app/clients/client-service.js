7875e035341ae736a8dbf31680881948753f5b07
Pontjho Mokhonoana <pontjho@entelect.co.za>
2017-10-18 19:53:35 +0200

    Also do it for the service

--- client2/app/clients/client-service.js
+++ client2/app/clients/client-service.js
@@ -1,7 +1,12 @@
-angular.module('absa').factory('ClientService', ['$resource', function($resource) {
-    return $resource('http://localhost:5000/api/clients/:clientId', {clientId:'@id'},
-    {
-        'update': { method:'PUT' },
-        'create': { method:'POST' }
-    });
-  }]);
+(function() {
+    "use strict";
+
+    angular.module('absa').factory('ClientService', ['$resource', function($resource) {
+        return $resource('http://localhost:5000/api/clients/:clientId', {clientId:'@id'},
+        {
+            'update': { method:'PUT' },
+            'create': { method:'POST' }
+        });
+      }]);
+
+})();
